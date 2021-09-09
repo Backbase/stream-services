f879d227749766e6295b9f2bf847c8c9be0e491b
Bart Veenstra <bartv@backbase.com>
2021-09-09 11:09:32 +0200

    Added spring configuration support to batch and tasks

--- stream-transactions/transactions-sink/src/main/java/com/backbase/stream/transaction/sink/TransactionSinkConfiguration.java
+++ stream-transactions/transactions-sink/src/main/java/com/backbase/stream/transaction/sink/TransactionSinkConfiguration.java
@@ -4,13 +4,18 @@ import com.backbase.dbs.transaction.api.service.v2.model.TransactionsPostRequest
 import com.backbase.dbs.transaction.api.service.v2.model.TransactionsPostResponseBody;
 import com.backbase.stream.TransactionService;
 import com.backbase.stream.transaction.TransactionTask;
+import com.backbase.stream.worker.exception.StreamTaskException;
 import com.backbase.stream.worker.model.UnitOfWork;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.function.context.config.JsonMessageConverter;
+import org.springframework.cloud.function.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeType;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -32,15 +37,19 @@ public class TransactionSinkConfiguration {
             Flux<TransactionsPostRequestBody> payload = messageFlux.map(Message::getPayload);
             Flux<UnitOfWork<TransactionTask>> processTransactions = transactionService.processTransactions(payload);
             return processTransactions
-                .doOnNext(transactionTaskUnitOfWork -> log.info("Processed unit of work: {}", transactionTaskUnitOfWork))
-                .onErrorResume(Exception.class, error -> {
-                    log.error("Failed to ingest transactions: {}", error.getMessage(), error);
-                    return Mono.error(error);
+                .onErrorResume(StreamTaskException.class, throwable -> {
+                    log.error("Failed to ingest transactions: {}", throwable.getMessage());
+                    return Mono.empty();
                 })
                 .then()
                 .map(MessageBuilder::withPayload)
                 .map(MessageBuilder::build);
         };
+    }
+
+    @Bean
+    public MessageConverter customMessageConverter(JsonMapper jsonMapper) {
+        return new JsonMessageConverter(jsonMapper, new MimeType("text", "plain"));
     }
 
 }
