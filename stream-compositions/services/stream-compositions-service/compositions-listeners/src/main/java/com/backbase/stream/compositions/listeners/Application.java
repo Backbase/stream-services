901b27c6b0705ecddf684e7c2d55701fc6d964e3
Piotr Marat <piotr@backbase.com>
2021-11-25 14:59:56 +0100

    Updated initial structure of composition module.

---
+++ stream-compositions/services/stream-compositions-service/compositions-listeners/src/main/java/com/backbase/stream/compositions/listeners/Application.java
@@ -0,0 +1,12 @@
+package com.backbase.stream.compositions.listeners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
