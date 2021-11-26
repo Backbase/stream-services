d10413e487726cea091af1abc616c20528fe07ff
Piotr Marat <piotr@backbase.com>
2021-11-26 18:25:12 +0100

    Added *failed events.

--- stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/model/LegalEntityIngestPullRequest.java
+++ stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/model/LegalEntityIngestPullRequest.java
@@ -1,9 +1,12 @@
 package com.backbase.stream.compositions.legalentity.model;
 
+import com.backbase.stream.legalentity.model.LegalEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Builder
 @AllArgsConstructor
 public class LegalEntityIngestPullRequest {
+    private String eventId;
+    private LegalEntity legalEntity;
 }
