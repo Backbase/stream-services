d90c9546a2a0b48e569365e87a93ee58b4d32676
Piotr Marat <piotr@backbase.com>
2021-11-27 00:06:52 +0100

    Updated initial structure.

---
+++ stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/configuration/IntegrationConfigurationProperties.java
@@ -0,0 +1,25 @@
+package com.backbase.stream.compositions.configuration;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@NoArgsConstructor
+@ConfigurationProperties("backbase.stream.compositions.integration")
+public class IntegrationConfigurationProperties {
+    /**
+     * Location of legal-entity-integration service.
+     */
+    private String legalEntityIngestionUrl = "http://legal-entity-ingestion-integration:8080";
+
+    /**
+     * Location of product-integration service.
+     */
+    private String productIngestionUrl = "http://product-ingestion-integration:8080";
+
+    /**
+     * Location of transaction-integration service.
+     */
+    private String transactionIngestionUrl = "http://transaction-ingestion-integration:8080";
+}
--- stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityIntegrationConfiguration.java
+++ stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityIntegrationConfiguration.java
@@ -1,7 +1,13 @@
 package com.backbase.stream.compositions.legalentity;
 
+import com.backbase.stream.compositions.configuration.IntegrationConfigurationProperties;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@AllArgsConstructor
+@EnableConfigurationProperties(IntegrationConfigurationProperties.class)
 public class LegalEntityIntegrationConfiguration {
+    private final IntegrationConfigurationProperties integrationConfigurationProperties;
 }
