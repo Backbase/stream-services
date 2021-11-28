95b9c9d85c5dcc1121ce01344c6a3c2f229e644a
Piotr Marat <piotr@backbase.com>
2021-11-28 01:44:02 +0100

    Added first draft of integration service for legal entity.

--- stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityIntegrationService.java
+++ stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityIntegrationService.java
@@ -1,5 +1,9 @@
 package com.backbase.stream.compositions.legalentity;
 
+import com.backbase.compositions.integration.legalentity.api.service.v1.LegalEntityIntegrationApi;
+import com.backbase.compositions.integration.legalentity.api.service.v1.model.GetLegalEntityRequest;
+import com.backbase.compositions.integration.legalentity.api.service.v1.model.GetLegalEntityResponse;
+import com.backbase.stream.compositions.legalentity.mapper.LegalEntityMapper;
 import com.backbase.stream.compositions.legalentity.model.LegalEntityIngestPullRequest;
 import com.backbase.stream.legalentity.model.LegalEntity;
 import lombok.AllArgsConstructor;
@@ -10,7 +14,22 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class LegalEntityIntegrationService {
-    public LegalEntity retrieveLegalEntity(LegalEntityIngestPullRequest request) {
-        return null;
+    private final LegalEntityMapper mapper;
+    private final LegalEntityIntegrationApi legalEntityIntegrationApi;
+
+    public LegalEntity retrieveLegalEntity(LegalEntityIngestPullRequest ingestPullRequest) {
+        GetLegalEntityRequest request = prepareRequest(ingestPullRequest);
+        GetLegalEntityResponse response = executeRequest(request);
+
+        return mapper.reMap(response.getLegalEntity());
+    }
+
+    private GetLegalEntityRequest prepareRequest(LegalEntityIngestPullRequest ingestPullRequest) {
+        return new GetLegalEntityRequest()
+                .legalEntity(mapper.reMap(ingestPullRequest.getLegalEntity()));
+    }
+
+    private GetLegalEntityResponse executeRequest(GetLegalEntityRequest request) {
+        return legalEntityIntegrationApi.getlegalEntity(request).block();
     }
 }
--- stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/model/LegalEntityIngestPullRequest.java
+++ stream-compositions/services/stream-compositions-service/compositions-core/src/main/java/com/backbase/stream/compositions/legalentity/model/LegalEntityIngestPullRequest.java
@@ -3,7 +3,9 @@ package com.backbase.stream.compositions.legalentity.model;
 import com.backbase.stream.legalentity.model.LegalEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class LegalEntityIngestPullRequest {
