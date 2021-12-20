319d9f837694b973a32efc68bbfd70612157b273
Piotr M <90273609+BB-piotr@users.noreply.github.com>
2021-12-20 16:55:40 +0100

    Feature/stro 15 legal entity composition (#87)
    Added stream-compositions (with legal entity submodules).

--- stream-compositions/services/legal-entity-composition-service/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityCompositionApplication.java
+++ stream-compositions/services/legal-entity-composition-service/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityCompositionApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import reactor.core.publisher.Hooks;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,6 +11,5 @@ import reactor.core.publisher.Hooks;
 public class LegalEntityCompositionApplication {
     public static void main(String[] args) {
         SpringApplication.run(LegalEntityCompositionApplication.class, args);
-        Hooks.onOperatorDebug();
     }
 }
