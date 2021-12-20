6d1a2ebdfeeac083c74923fe5c48fc445118c31f
Piotr Marat <piotr@backbase.com>
2021-12-20 18:18:19 +0100

    Updated product-composition tests.

--- stream-compositions/services/product-composition-service/src/main/java/com/backbase/stream/compositions/product/core/config/BootstrapConfigurationProperties.java
+++ stream-compositions/services/product-composition-service/src/main/java/com/backbase/stream/compositions/product/core/config/BootstrapConfigurationProperties.java
@@ -1,6 +1,6 @@
 package com.backbase.stream.compositions.product.core.config;
 
-import com.backbase.stream.productcatalog.model.ProductCatalog;
+import com.backbase.stream.legalentity.model.ProductGroup;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,5 +9,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @NoArgsConstructor
 @ConfigurationProperties(prefix = "bootstrap", ignoreInvalidFields = true)
 public class BootstrapConfigurationProperties {
-    private ProductCatalog productCatalog;
+    private ProductGroup productCatalog;
 }
