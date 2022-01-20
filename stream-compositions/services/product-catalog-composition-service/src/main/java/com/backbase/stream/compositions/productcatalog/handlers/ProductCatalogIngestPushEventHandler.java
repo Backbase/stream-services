a8c993db0ec7f0425b01f521ee1d9b96fb8ecd2d
Piotr Marat <piotr@backbase.com>
2022-01-20 19:59:42 +0100

    Added product-catalog-composition.

---
+++ stream-compositions/services/product-catalog-composition-service/src/main/java/com/backbase/stream/compositions/productcatalog/handlers/ProductCatalogIngestPushEventHandler.java
@@ -0,0 +1,35 @@
+package com.backbase.stream.compositions.productcatalog.handlers;
+
+import com.backbase.buildingblocks.backend.communication.event.EnvelopedEvent;
+import com.backbase.buildingblocks.backend.communication.event.handler.EventHandler;
+import com.backbase.com.backbase.stream.compositions.events.ingress.event.spec.v1.ProductCatalogIngestPushEvent;
+import com.backbase.stream.compositions.productcatalog.core.model.ProductCatalogIngestPushRequest;
+import com.backbase.stream.compositions.productcatalog.core.service.ProductCatalogIngestionService;
+import com.backbase.stream.compositions.productcatalog.mapper.ProductCatalogMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@AllArgsConstructor
+public class ProductCatalogIngestPushEventHandler implements EventHandler<ProductCatalogIngestPushEvent> {
+    private final ProductCatalogIngestionService productCatalogIngestionService;
+    private final ProductCatalogMapper mapper;
+
+    @Override
+    public void handle(EnvelopedEvent<ProductCatalogIngestPushEvent> envelopedEvent) {
+        productCatalogIngestionService.ingestPush(buildRequest(envelopedEvent));
+    }
+
+    /**
+     * Builds ingestion request for downstream service.
+     *
+     * @param envelopedEvent EnvelopedEvent<LegalEntityIngestPushEvent>
+     * @return LegalEntityIngestPullRequest
+     */
+    private Mono<ProductCatalogIngestPushRequest> buildRequest(EnvelopedEvent<ProductCatalogIngestPushEvent> envelopedEvent) {
+        return Mono.just(ProductCatalogIngestPushRequest.builder()
+                .productCatalog(mapper.mapEventToStream(envelopedEvent.getEvent().getProductCatalog()))
+                .build());
+    }
+}
