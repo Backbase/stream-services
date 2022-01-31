f49e22ea327a707426bc74eeefbd1ec284fc362a
Piotr M <90273609+BB-piotr@users.noreply.github.com>
2022-01-31 16:10:25 +0100

    Added  product composition (#99)
    * Added product-composition.
    
    * Updated mapper ingestion strategy.

---
+++ stream-compositions/services/product-composition-service/src/main/java/com/backbase/stream/compositions/product/core/config/ProductCompositionConfiguration.java
@@ -0,0 +1,46 @@
+package com.backbase.stream.compositions.product.core.config;
+
+import com.backbase.stream.compositions.integration.product.ApiClient;
+import com.backbase.stream.compositions.integration.product.api.ProductIntegrationApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.text.DateFormat;
+
+@Configuration
+@AllArgsConstructor
+@EnableConfigurationProperties(ProductConfigurationProperties.class)
+public class ProductCompositionConfiguration {
+    private final ProductConfigurationProperties productConfigurationProperties;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public ProductIntegrationApi productIntegrationApi(ApiClient productClient) {
+        return new ProductIntegrationApi(productClient);
+    }
+
+    @Bean
+    public ApiClient productClient(
+            WebClient dbsWebClient,
+            ObjectMapper objectMapper,
+            DateFormat dateFormat) {
+        ApiClient apiClient = new ApiClient(dbsWebClient, objectMapper, dateFormat);
+        apiClient.setBasePath(productConfigurationProperties.getProductIntegrationUrl());
+
+        return apiClient;
+    }
+}
