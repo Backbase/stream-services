1f5921a0ba585d7b19b98d32df53a8d430323358
Sushil Joshi <sushil@backbase.com>
2022-05-02 10:47:26 -0400

    mock responses for Product And Legal Entity Integration

--- stream-compositions/services/product-composition-service/src/main/java/com/backbase/stream/compositions/product/core/service/impl/ProductIntegrationServiceImpl.java
+++ stream-compositions/services/product-composition-service/src/main/java/com/backbase/stream/compositions/product/core/service/impl/ProductIntegrationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.backbase.stream.compositions.product.core.service.impl;
 
 import com.backbase.stream.compositions.integration.product.api.ProductIntegrationApi;
+import com.backbase.stream.compositions.integration.product.model.AvailableBalance;
+import com.backbase.stream.compositions.integration.product.model.LegalEntityReference;
 import com.backbase.stream.compositions.integration.product.model.ProductGroup;
+import com.backbase.stream.compositions.integration.product.model.SavingsAccount;
 import com.backbase.stream.compositions.product.core.model.ProductIngestPullRequest;
 import com.backbase.stream.compositions.product.core.service.ProductIntegrationService;
 import lombok.AllArgsConstructor;
@@ -19,7 +22,16 @@ public class ProductIntegrationServiceImpl implements ProductIntegrationService 
      * {@inheritDoc}
      */
     public Mono<ProductGroup> pullProductGroup(ProductIngestPullRequest ingestPullRequest) {
-        return Mono.just(new ProductGroup().name("Some Name"));
+        LegalEntityReference leRef = new LegalEntityReference();
+        leRef.setExternalId(ingestPullRequest.getLegalEntityExternalId());
+        return Mono.just(new ProductGroup().name("Some Name")
+                .addSavingAccountsItem(new SavingsAccount()
+                        .name("Savings1")
+                        .addLegalEntitiesItem(leRef)
+                        .BBAN("bban")
+                        .externalId("account1")
+                        .productTypeExternalId("savings-account")
+                        .availableBalance(new AvailableBalance().amount(1.01).currencyCode("USD"))));
                 /*productIntegrationApi
                 .pullProductGroup(
                         ingestPullRequest.getLegalEntityExternalId(),
