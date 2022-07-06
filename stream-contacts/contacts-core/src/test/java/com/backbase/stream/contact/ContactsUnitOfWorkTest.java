36a2cae2a83ed93fa22ec570d273fba6c2dd2815
Vikas Grover <vikasg@backbase.com>
2022-07-06 08:50:11 -0700

    Unit Test coverage

---
+++ stream-contacts/contacts-core/src/test/java/com/backbase/stream/contact/ContactsUnitOfWorkTest.java
@@ -0,0 +1,72 @@
+package com.backbase.stream.contact;
+
+import com.backbase.dbs.contact.api.service.v2.model.AccessContextScope;
+import com.backbase.dbs.contact.api.service.v2.model.ContactsBulkPostRequestBody;
+import com.backbase.dbs.contact.api.service.v2.model.ExternalAccessContext;
+import com.backbase.dbs.contact.api.service.v2.model.ExternalAccountInformation;
+import com.backbase.dbs.contact.api.service.v2.model.ExternalContact;
+import com.backbase.dbs.contact.api.service.v2.model.IngestMode;
+import com.backbase.stream.configuration.ContactsServiceConfiguration;
+import com.backbase.stream.configuration.ContactsWorkerConfigurationProperties;
+import com.backbase.stream.contact.repository.ContactsUnitOfWorkRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Import;
+import reactor.core.publisher.Flux;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@Import(ContactsServiceConfiguration.class)
+class ContactsUnitOfWorkTest {
+
+    private ContactsUnitOfWorkExecutor contactsUnitOfWorkExecutor;
+
+    @Mock
+    private ContactsUnitOfWorkRepository repository;
+
+    @Mock
+    private ContactsSaga streamTaskExecutor;
+
+    @Mock
+    ContactsWorkerConfigurationProperties streamWorkerConfiguration;
+
+    @Test
+    void test_executeTask() {
+        when(streamWorkerConfiguration.getTaskExecutors()).thenReturn(1);
+        this.contactsUnitOfWorkExecutor = new ContactsUnitOfWorkExecutor(repository, streamTaskExecutor, streamWorkerConfiguration);
+        contactsUnitOfWorkExecutor.prepareUnitOfWork(Collections.singletonList(getContactsBulkPostRequestBody()));
+    }
+
+    @Test
+    void test_executeTask2() {
+        when(streamWorkerConfiguration.getTaskExecutors()).thenReturn(1);
+        when(streamWorkerConfiguration.getBufferSize()).thenReturn(2);
+        this.contactsUnitOfWorkExecutor = new ContactsUnitOfWorkExecutor(repository, streamTaskExecutor, streamWorkerConfiguration);
+
+        Flux<ContactsBulkPostRequestBody> contactsBulkPostRequestBodyFlux = Flux.just(getContactsBulkPostRequestBody());
+        contactsUnitOfWorkExecutor.prepareUnitOfWork(contactsBulkPostRequestBodyFlux);
+    }
+
+    private ContactsBulkPostRequestBody getContactsBulkPostRequestBody() {
+        var request = new ContactsBulkPostRequestBody();
+        request.setIngestMode(IngestMode.UPSERT);
+        ExternalAccessContext accessContext = new ExternalAccessContext();
+        accessContext.setScope(AccessContextScope.LE);
+        request.setAccessContext(accessContext);
+        ExternalContact contact = new ExternalContact();
+        contact.setName("TEST1");
+        contact.setExternalId("TEST101");
+        ExternalAccountInformation account = new ExternalAccountInformation();
+        account.setName("TESTACC1");
+        account.setExternalId("TESTACC101");
+        contact.setAccounts(Collections.singletonList(account));
+        request.setContacts(Collections.singletonList(contact));
+        return request;
+    }
+
+}
