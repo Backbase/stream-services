316e273f4015754d8889086bd543b1cbcb10b6f9
Vikas Grover <vikasg@backbase.com>
2022-07-14 00:18:51 -0700

    Code Coverage

--- stream-contacts/contacts-core/src/main/java/com/backbase/stream/configuration/ContactsWorkerConfigurationProperties.java
+++ stream-contacts/contacts-core/src/main/java/com/backbase/stream/configuration/ContactsWorkerConfigurationProperties.java
@@ -1,13 +1,15 @@
 package com.backbase.stream.configuration;
 
 import com.backbase.stream.worker.configuration.StreamWorkerConfiguration;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "backbase.stream.contact.worker")
 @Slf4j
-@Data
+@Getter
+@Setter
 public class ContactsWorkerConfigurationProperties extends StreamWorkerConfiguration {
     private boolean continueOnError;
 }
