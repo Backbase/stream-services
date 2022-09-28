933a62ed2f0a5056b62baa60ee5792ec55ce29f8
Jan <jan@backbase.com>
2022-09-28 09:44:02 -0400

    removing unnecessary comemnts

--- stream-compositions/services/payment-order-composition-service/src/main/java/com/backbase/stream/compositions/paymentorders/core/config/PaymentOrderConfigurationProperties.java
+++ stream-compositions/services/payment-order-composition-service/src/main/java/com/backbase/stream/compositions/paymentorders/core/config/PaymentOrderConfigurationProperties.java
@@ -13,7 +13,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class PaymentOrderConfigurationProperties {
     private String integrationBaseUrl = "http://payment-orders-ingestion-integration:8080";
     private Events events;
-//    private Cursor cursor;
     private Integer defaultStartOffsetInDays;
 
     @Data
@@ -22,20 +21,4 @@ public class PaymentOrderConfigurationProperties {
         private Boolean enableCompleted = Boolean.FALSE;
         private Boolean enableFailed = Boolean.FALSE;
     }
-//
-//    @Data
-//    @NoArgsConstructor
-//    public static class Cursor {
-//        private Boolean enabled = Boolean.FALSE;
-//        private Boolean transactionIdsFilterEnabled = Boolean.FALSE;
-//        private String baseUrl = "http://product-cursor:9000";
-//    }
-//
-//    public final boolean isCursorEnabled() {
-//        return Boolean.TRUE.equals(cursor.getEnabled());
-//    }
-//
-//    public final boolean isTransactionIdsFilterEnabled() {
-//        return Boolean.TRUE.equals(cursor.getTransactionIdsFilterEnabled());
-//    }
 }
