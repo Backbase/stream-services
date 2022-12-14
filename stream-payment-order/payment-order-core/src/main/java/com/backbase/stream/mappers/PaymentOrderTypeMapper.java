b6ba71a40a42eda12f8e4915a5bc19ef28c12968
Jan <jan@backbase.com>
2022-12-14 17:26:30 +0200

    cherry picking commit dc0b53cd3df54585af5e7db5526e4d40b83fd321

--- stream-payment-order/payment-order-core/src/main/java/com/backbase/stream/mappers/PaymentOrderTypeMapper.java
+++ stream-payment-order/payment-order-core/src/main/java/com/backbase/stream/mappers/PaymentOrderTypeMapper.java
@@ -4,12 +4,14 @@ import com.backbase.dbs.paymentorder.api.service.v2.model.GetPaymentOrderRespons
 import com.backbase.dbs.paymentorder.api.service.v2.model.PaymentOrderPostRequest;
 import com.backbase.dbs.paymentorder.api.service.v2.model.PaymentOrderPutRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PaymentOrderTypeMapper {
 
+    @Mapping(source = "schedule.nextExecutionDate", target = "nextExecutionDate")
     PaymentOrderPutRequest mapPaymentOrderPostRequest(PaymentOrderPostRequest paymentOrderPostRequest);
 
     List<PaymentOrderPostRequest> mapPaymentOrderPostRequest(List<GetPaymentOrderResponse> paymentOrderPostRequest);
