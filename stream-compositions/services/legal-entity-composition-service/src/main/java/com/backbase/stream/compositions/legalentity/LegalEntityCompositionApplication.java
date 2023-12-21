2c912d9e02b71d009239dbb6840d3c4e7b177afc
Roman Kniazevych <aboruhen@gmail.com>
2023-12-21 11:11:16 +0200

    remove sleuth for composition

--- stream-compositions/services/legal-entity-composition-service/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityCompositionApplication.java
+++ stream-compositions/services/legal-entity-composition-service/src/main/java/com/backbase/stream/compositions/legalentity/LegalEntityCompositionApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
 import reactor.core.publisher.Hooks;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan("com.backbase.stream")
+//@ComponentScan(
+//    value = "com.backbase.stream",
+//    excludeFilters = @Filter(type = FilterType.REGEX,
+//        pattern = "com\\.backbase\\.stream\\.compositions\\.events\\.egress\\.event\\.spec\\.v1..*")
+//)
 public class LegalEntityCompositionApplication {
 
     public static void main(String[] args) {
