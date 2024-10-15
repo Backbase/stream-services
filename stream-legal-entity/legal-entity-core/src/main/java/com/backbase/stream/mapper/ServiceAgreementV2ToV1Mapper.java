2a14e1dd70a64c370a1266ee4c8b55e35b005f70
Olena Chubukina <olenac@backbase.com>
2024-10-15 16:19:38 +0200

    DAS-1399 - add purpose field to service agreement

--- stream-legal-entity/legal-entity-core/src/main/java/com/backbase/stream/mapper/ServiceAgreementV2ToV1Mapper.java
+++ stream-legal-entity/legal-entity-core/src/main/java/com/backbase/stream/mapper/ServiceAgreementV2ToV1Mapper.java
@@ -15,6 +15,7 @@ public interface ServiceAgreementV2ToV1Mapper {
         @Mapping(target = "externalId", source = "externalId")
         @Mapping(target = "name", source = "name")
         @Mapping(target = "description", source = "description")
+        @Mapping(target = "purpose", source = "purpose")
         @Mapping(target = "validFromDate", source = "validFromDate")
         @Mapping(target = "validFromTime", source = "validFromTime")
         @Mapping(target = "validUntilDate", source = "validUntilDate")
