b1738dba8ddd7a7a041647c164dba9489cec9ee5
mnaimisha <81910467+mnaimisha@users.noreply.github.com>
2025-02-27 19:33:16 +0530

    Update AccessControlConfigurationProperties.java

--- stream-access-control/access-control-core/src/main/java/com/backbase/stream/configuration/AccessControlConfigurationProperties.java
+++ stream-access-control/access-control-core/src/main/java/com/backbase/stream/configuration/AccessControlConfigurationProperties.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Setter
 @Getter
 @ConfigurationProperties("backbase.stream.access.control")
+@Validated
 public class AccessControlConfigurationProperties {
 
     @Min(1)
