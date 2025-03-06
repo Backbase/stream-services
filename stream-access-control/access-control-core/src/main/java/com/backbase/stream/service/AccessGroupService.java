ff823204d7dfc14fdfd1fb2f32b8f573db781477
Olena Chubukina <olenac@backbase.com>
2025-03-06 10:21:33 +0100

    NOJIRA - APS should be added only to TEMPLATE job roles

--- stream-access-control/access-control-core/src/main/java/com/backbase/stream/service/AccessGroupService.java
+++ stream-access-control/access-control-core/src/main/java/com/backbase/stream/service/AccessGroupService.java
@@ -1256,8 +1256,10 @@ public class AccessGroupService {
             presentationIngestFunctionGroup.setType(PresentationIngestFunctionGroup.TypeEnum.REGULAR);
         }
 
-        // Removing constant from mapper and adding default APS here to avoid issues with apsName.
-        if(jobRole.getApsId() == null && isEmpty(jobRole.getApsName())){
+        // Removing constant from mapper and adding default APS here to avoid issues with apsName
+        //Add default APS only to TEMPLATE Job roles
+        if (jobRole.getApsId() == null && isEmpty(jobRole.getApsName())
+            && PresentationIngestFunctionGroup.TypeEnum.TEMPLATE.equals(presentationIngestFunctionGroup.getType())) {
             log.warn("Adding default APS '1 - User APS' to job role since it wasn't previously set.");
             presentationIngestFunctionGroup.setApsId(1L);
         }
