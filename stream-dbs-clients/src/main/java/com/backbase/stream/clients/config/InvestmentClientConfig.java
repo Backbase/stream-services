171fb07de5307d6e63d491adfc304b4f44c95dab
Roman Kniazevych <aboruhen@gmail.com>
2025-11-25 13:46:08 +0200

    fix webclient api configs for investment api's

--- stream-dbs-clients/src/main/java/com/backbase/stream/clients/config/InvestmentClientConfig.java
+++ stream-dbs-clients/src/main/java/com/backbase/stream/clients/config/InvestmentClientConfig.java
@@ -7,15 +7,18 @@ import com.backbase.investment.api.service.v1.FinancialAdviceApi;
 import com.backbase.investment.api.service.v1.InvestmentProductsApi;
 import com.backbase.investment.api.service.v1.PortfolioApi;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.text.DateFormat;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 /**
  * Configuration for Investment service REST client (ClientApi).
@@ -35,13 +38,18 @@ public class InvestmentClientConfig extends CompositeApiClientConfig {
      */
     @Bean
     @ConditionalOnMissingBean
-    public ApiClient investmentApiClient(ObjectMapper objectMapper, DateFormat dateFormat) {
+    public ApiClient investmentApiClient(WebClient webClient, ObjectMapper objectMapper, DateFormat dateFormat) {
         ObjectMapper mapper = objectMapper.copy();
-        objectMapper.setSerializationInclusion(Include.NON_EMPTY);
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.setSerializationInclusion(Include.NON_EMPTY);
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return new ApiClient(getWebClient(), mapper, dateFormat)
+        Builder mutate = webClient.mutate();
+        mutate.codecs(clientCodecConfigurer -> {
+            Jackson2JsonEncoder encoder = new Jackson2JsonEncoder(mapper, MediaType.APPLICATION_JSON);
+            Jackson2JsonDecoder decoder = new Jackson2JsonDecoder(mapper, MediaType.APPLICATION_JSON);
+            clientCodecConfigurer.defaultCodecs().jackson2JsonEncoder(encoder);
+            clientCodecConfigurer.defaultCodecs().jackson2JsonDecoder(decoder);
+        });
+        return new ApiClient(mutate.build(), mapper, dateFormat)
             .setBasePath(createBasePath());
     }
 
